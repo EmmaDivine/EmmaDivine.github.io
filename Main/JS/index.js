5eb88ae1929f471e6ce11b859ff462e54879e8e4
Divine <97335481+Emmily006@users.noreply.github.com>
2022-12-12 17:37:53 +0100

    A

--- Main/JS/index.js
+++ Main/JS/index.js
@@ -1,19 +1,18 @@
+var audio = new Audio('../Main/MP3/Nyan.mp3');
+
 function agree() {
     document.getElementById('welcome').style.display = "none";
-    var audio = new Audio('../Main/MP3/Nyan.mp3');
     audio.play();
 }
 
 setInterval(agree, 216000)
 
 function aud1() {
-    var audio = new Audio('../Main/MP3/Nyan.mp3');
     audio.pause();
 
 }
 
 function aud2() {
-    var audio = new Audio('../Main/MP3/Nyan.mp3');
     audio.play();
 
 }
