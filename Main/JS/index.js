948c82a6013ba43922bd81339b22cbbbc5456013
Divine <97335481+Emmily006@users.noreply.github.com>
2023-01-11 13:36:11 +0100

    Update index.js

--- Main/JS/index.js
+++ Main/JS/index.js
@@ -3,9 +3,8 @@ var audio = new Audio('../Main/MP3/Nyan.mp3');
 function agree() {
     document.getElementById('welcome').style.display = "none";
     audio.play();
+    setInterval(agree, 216000)
 }
-
-setInterval(agree, 216000)
 
 function aud1() {
     audio.pause();
